a4584399fd52041984ee61b5f7e99f0aa0d38e5b
Godswill Amos <amosgodswill00@gmail.com>
2025-07-04 00:34:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-from fastapi import FastAPI, middleware
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import verify
+
 
 app = FastAPI()
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],            # or ["*"] to allow all (not recommended for production)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(verify.router)
+
+@app.get("/ping")
+async def ping_server():
+    return "server active"
